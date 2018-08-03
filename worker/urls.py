9adf493dd984e71056d10ad8a935a0fb36c233d8
Kaushal <kaushal@juggernaut.in>
2018-08-03 16:37:59 +0530

    worker task list

--- worker/urls.py
+++ worker/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('task-list', views.TaskList.as_view(),
+         name='worker_task_list'),
     path('', views.index, name='worker_auth_index'),
+
 ]
--- worker/views.py
+++ worker/views.py
@@ -6,6 +6,34 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+from django.db.models import Q
+
+from django.http import HttpResponse
+
+from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+
+
+
+from django.http import *
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.views import View
+
+from consumer.models import ConsumerTask
 
 from django.http import HttpResponse
 
@@ -13,3 +41,20 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the worker index.")
 
+class TaskList(View):
+    def get(self, request):
+        template = loader.get_template('worker/task_list.html')
+        context = {}
+        tasks = ConsumerTask.objects.filter(~Q(status=ConsumerTask.COMPLETED))
+        context['tasks'] = tasks
+        return HttpResponse(template.render(context, request))
+
+
+# class CompletedTaskList(View):
+#     def get(self, request):
+#         template = loader.get_template('worker/task_list.html')
+#         context = {}
+#         tasks = ConsumerTask.objects.filter(user=request.user, status=ConsumerTask.COMPLETED)
+#         context['tasks'] = tasks
+#         context['status'] = 'completed'
+#         return HttpResponse(template.render(context, request))
