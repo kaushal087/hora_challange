571456c0f6f79e1c6903042b73c76b1432632e18
Kaushal <kaushal@juggernaut.in>
2018-08-03 14:21:50 +0530

    add basic templates

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,11 +1,52 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
-
+from django.views import View
 
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the users index.")
+
+
+
+class SignUpView(View):
+
+    def get(self, request):
+        template = loader.get_template('user_auth/signup.html')
+        context = {}
+        return HttpResponse(template.render(context, request))
+
+
+
+class WorkerSignUpView(View):
+
+    def get(self, request):
+        template = loader.get_template('user_auth/worker_signup.html')
+        context = {}
+        return HttpResponse(template.render(context, request))
+
+
+    def post(self, request):
+        pass
+
+
+class ConsumerSignUpView(View):
+
+    def get(self, request):
+        template = loader.get_template('user_auth/consumer_signup.html')
+        context = {}
+        return HttpResponse(template.render(context, request))
+
+
+    def post(self, request):
+        pass
 
